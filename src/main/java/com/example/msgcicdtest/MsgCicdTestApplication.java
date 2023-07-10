d94d01f43eb388e56c09b8862022954fd154e599
MinnSeoo <ms.rry01@gmail.com>
2023-07-11 07:59:32 +0900

    project setting

---
+++ src/main/java/com/example/msgcicdtest/MsgCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgcicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgCicdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgCicdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msgcicdtest/MsgCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgcicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgCicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
