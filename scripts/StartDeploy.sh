93cff5bcc466e7b8d45111e971159508cf0d66e3
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2023-07-25 13:22:48 +0900

    Update StartDeploy.sh

--- scripts/StartDeploy.sh
+++ scripts/StartDeploy.sh
@@ -2,16 +2,16 @@ BUILD_JAR=$(ls /home/ec2-user/CI-CD-Test/build/libs/*.jar)  # 프로젝트 내 j
 JAR_NAME=$(basename $BUILD_JAR) # 위에서 빌드한 jar 파일을 저장
 echo "> build 파일명: $JAR_NAME" >>/home/ec2-user/CI-CD-Test/build/libs/*.jar
 
-echo "> build 파일 복사" >> /home/ec2-user/script/StartDeploy.sh
+echo "> build 파일 복사" >> /var/log/aws/codedeploy-agent/codedeploy-agent.log
 DEPLOY_PATH=/home/ec2-user/script/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >>/home/ec2-user/CI-CD-Test/build/libs/*.jar
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /var/log/aws/codedeploy-agent/codedeploy-agent.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >>/home/ec2-user/CI-CD-Test/build/libs/*.jar
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> cat /var/log/aws/codedeploy-agent/codedeploy-agent.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -20,4 +20,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/script/StartDeploy.sh
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/CI-CD-Test/build/libs/*.jar &
+nohup java -jar $DEPLOY_JAR >> /var/log/aws/codedeploy-agent/codedeploy-agent.log
